919de281310aab4d58d60805989c8caf800186ba
printersdriverr <67159107+printersdriverr@users.noreply.github.com>
2021-02-07 20:21:04 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
-POOL=stratum+tcp://eth.f2pool.com:6688
-WALLET=0x70278496f0eaa3810d9dbdd7f388425e029013db
-WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )-lol
 
-cd "$(dirname "$0")"
 
-chmod +x ./miner && ./miner --algo ethash --server $POOL --user $WALLET.$WORKER --tls 0 $@ --4g-alloc-size 4076
+#!/bin/sh
+./miner --algo ethash --server stratum+tcp://eth.f2pool.com:6688 --user 0x70278496f0eaa3810d9dbdd7f388425e029013db
