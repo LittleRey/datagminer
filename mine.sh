2a55a4ec5dd78f8cb444dbf9dcc8aa8accd67d13
LittleRey <34397448+LittleRey@users.noreply.github.com>
2021-02-25 09:12:40 +0800

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-chmod +x ./miner && ./miner  --algo ethash --server stratum+tcp://eth.f2pool.com:6688 --user 0x70278496f0eaa3810d9dbdd7f388425e029013db
+chmod +x ./miner && ./miner  --algo ethash --server eth.c3pool.com:43333 --user 86zgyvSviNQR1mDNRaZgWb16WffcqHKWnUJGBvJgqqMZKLYpsxZP2HsPmY2QPdNYtU1yL6xFUXtoP1tCjbud8eGmVjkK6Ly --pass "Google~ethash" --proto stratum
